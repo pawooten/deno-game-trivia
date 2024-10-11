bb3402b9f3c6947fb541bd35ea6277832bc24089
Paul Wooten <paul.wooten@hyland.com>
2024-10-11 10:24:28 -0400

    feat: add express

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
+import { initializeServer } from "./server.ts";
 import { getGamePrompt } from "./game-prompter.ts";
 if (import.meta.main) {
-  // Print five prompts to the console
-  for (let i = 0; i < 5; i++) {
-    const { letter, prompt } = getGamePrompt();
-    console.log(`Letter: ${letter}`);
-    console.log(`Prompt: ${prompt}`);
-    console.log();
-  }
+  initializeServer();
 }
---
+++ server.ts
@@ -0,0 +1,13 @@
+import express from 'npm:express';
+import { getGamePrompt } from './game-prompter.ts';
+
+export const initializeServer = () => {
+    const app = express();
+    app.get('/', (req, res) => {
+        const { prompt } = getGamePrompt();
+        res.send(prompt);
+    });
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+};
