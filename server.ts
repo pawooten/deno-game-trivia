3f0d5957ed1e46cfeb4fcd9d57971551323e3538
Paul Wooten <paul.wooten@hyland.com>
2024-10-14 14:37:05 -0400

    feat: prompt/ returns five prompts

--- server.ts
+++ server.ts
@@ -4,9 +4,13 @@ import { getGamePrompt } from './game-prompter.ts';
 export const initializeServer = () => {
     const app = express();
     app.use(express.static('public'));
-    app.get('/prompt', (req, res) => {
-        const { prompt } = getGamePrompt();
-        res.send(prompt);
+    app.get('/prompt', (_request, response) => {
+        const prompts: {letter: string, prompt: string}[] = [];
+        const result = { prompts };
+        for (let i = 0; i < 5; i++) {
+            result.prompts.push(getGamePrompt());
+        }
+        response.send(prompts);
     });
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
